823328c04479cbe0adc12212474ffb681e708516
RubenTei <rubenmenendezgarcia@gmail.com>
2019-07-10 15:46:29 +0200

    added buttons in map

--- src/app/home/maps/maps.page.ts
+++ src/app/home/maps/maps.page.ts
@@ -1,165 +1,112 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
- 
+
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
- 
+import { MarkerOptions, LatLng } from '@ionic-native/google-maps';
+import { PlaceService } from 'src/app/place.service';
+
 declare var google;
- 
+
 @Component({
-    selector: 'app-maps',
-    templateUrl: './maps.page.html',
-    styleUrls: ['./maps.page.scss'],
-  })
+  selector: 'app-maps',
+  templateUrl: './maps.page.html',
+  styleUrls: ['./maps.page.scss'],
+})
 export class MapsPage {
- 
+  public places = [];
+
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  address:string;
- 
+  address: string;
+
   constructor(
     private geolocation: Geolocation,
-    private nativeGeocoder: NativeGeocoder) {
+    private nativeGeocoder: NativeGeocoder,
+    private placeService: PlaceService) {
   }
- 
- 
+
+
   ngOnInit() {
     this.loadMap();
+    this.places = this.placeService.getPlaces();
   }
- 
+
   loadMap() {
     this.geolocation.getCurrentPosition().then((resp) => {
       let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
       let mapOptions = {
         center: latLng,
-        zoom: 14,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 13,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
       }
- 
       this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
- 
+
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
+
       this.map.addListener('tilesloaded', () => {
-        console.log('accuracy',this.map);
+        console.log('accuracy', this.map);
         this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
       });
- 
+
     }).catch((error) => {
       console.log('Error getting location', error);
     });
+
+    this.addMarkerCurrentPos();
   }
- 
+
   getAddressFromCoords(lattitude, longitude) {
-    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    console.log("getAddressFromCoords " + lattitude + " " + longitude);
     let options: NativeGeocoderOptions = {
       useLocale: true,
       maxResults: 5
     };
- 
+
     this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
       .then((result: NativeGeocoderReverseResult[]) => {
         this.address = "";
         let responseAddress = [];
         for (let [key, value] of Object.entries(result[0])) {
-          if(value.length>0)
-          responseAddress.push(value);
- 
+          if (value.length > 0)
+            responseAddress.push(value);
+
         }
         responseAddress.reverse();
         for (let value of responseAddress) {
-          this.address += value+", ";
+          this.address += value + ", ";
         }
         this.address = this.address.slice(0, -2);
       })
-      .catch((error: any) =>{
+      .catch((error: any) => {
         this.address = "Address Not Available!";
       });
- 
   }
- 
- 
+
+  addMarkerCurrentPos() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      const icon = "https://img.icons8.com/dusk/64/000000/street-view.png";
+      const animation = google.maps.Animation.DROP;
+      const position = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      const marker = new google.maps.Marker({ position, title: 'Estás aquí!', animation: animation, icon: icon });
+      marker.setMap(this.map);
+    });
+  }
+
+  
+  getMarkers() {
+    // tslint:disable-next-line:variable-name
+    for (let _i = 0; _i < this.places.length; _i++) {
+      if (_i > 0) {
+        console.log(this.places.length);
+        this.addMarkersToMap(this.places[_i]);
+      }
+    }
+  }
+
+  addMarkersToMap(place) {
+    const position = new google.maps.LatLng(place.latitude, place.longitude);
+    const placeMarker = new google.maps.Marker({ position, title: place.name });
+    placeMarker.setMap(this.map);
+  }
 }
-
-// import { Component, OnInit, ViewChild} from '@angular/core';
-
-// import { Geolocation } from '@ionic-native/geolocation/ngx';
-// import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
-// import {
-//   GoogleMaps,
-//   GoogleMap,
-//   GoogleMapsEvent,
-//   GoogleMapOptions,
-//   CameraPosition,
-//   MarkerOptions,
-//   Marker,
-//   Environment
-// } from '@ionic-native/google-maps';
-
-// declare var google;
-
-// @Component({
-//   selector: 'app-maps',
-//   templateUrl: './maps.page.html',
-//   styleUrls: ['./maps.page.scss'],
-// })
-// export class MapsPage implements OnInit {
-//   map;
-
-//   @ViewChild('mapElement') mapElement;
-//   constructor(
-//     private geolocation: Geolocation,
-//     private nativeGeocoder: NativeGeocoder
-//   ) { }
-
-//   ngOnInit() {
- 
-//   }
-
-//   ngAfterContentInit(): void {
-//     this.map = new google.maps.Map(
-//       this.mapElement.nativeElement,
-//       {
-//         center: {lat: -34.397, lng: 150.644},
-//         zoom: 8
-//       });
-//   }
-
-//   ionViewDidLoad() {
-//     this.loadMap();
-//   }
-
-//   loadMap() {
-
-//     // This code is necessary for browser
-//     Environment.setEnv({
-//       'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDeS0aNeEcW1qeCHJgyEGuVM3E6lH0MjK0',
-//       'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDeS0aNeEcW1qeCHJgyEGuVM3E6lH0MjK0'
-//     });
-
-//     let mapOptions: GoogleMapOptions = {
-//       camera: {
-//          target: {
-//            lat: 43.0741904,
-//            lng: -89.3809802
-//          },
-//          zoom: 18,
-//          tilt: 30
-//        }
-//     };
-
-//     this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-//     let marker: Marker = this.map.addMarkerSync({
-//       title: 'Ionic',
-//       icon: 'blue',
-//       animation: 'DROP',
-//       position: {
-//         lat: 43.0741904,
-//         lng: -89.3809802
-//       }
-//     });
-//     marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-//       alert('clicked');
-//     });
-//   }
-// }
--- src/app/place.service.ts
+++ src/app/place.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaceService {
 
+  private places: any = [
+    { latitude: 40.46628036112748, longitude: -3.648011692639075, title: 'PARQUE1' }
+  ]
+  static places: any;
   constructor() { }
+
+  public getPlaces(): any {
+
+    return this.places;
+  }
 }
