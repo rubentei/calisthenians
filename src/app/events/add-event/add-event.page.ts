68903f478eb37fef9b1163ddd9ada2adfa8e41d4
Arya <vallhala@hotmail.com>
2019-07-10 17:32:07 +0200

    change

--- src/app/events/add-event/add-event.page.ts
+++ src/app/events/add-event/add-event.page.ts
@@ -4,12 +4,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-event.page.html',
   styleUrls: ['./add-event.page.scss'],
 })
-export class AddEventPage implements OnInit {
+export class AddEventPage {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  customYearValues = [2020, 2016, 2008, 2004, 2000, 1996];
+  customDayShortNames = ['s\u00f8n', 'man', 'tir', 'ons', 'tor', 'fre', 'l\u00f8r'];
+  customPickerOptions: any;
 
   constructor() { 
     
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: () => console.log('Clicked Save!')
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
   }
 
-  ngOnInit() {}
+  }
 
-}
