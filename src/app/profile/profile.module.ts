e6d0d69060d94cd7d951355935cf9325f653ced2
Rich Lund <styrka25@hotmail.com>
2019-07-12 17:41:40 +0200

    hola

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePage } from './profile.page';
+import { EventsListPage } from '../events-list/events-list.page';
+
 
 const routes: Routes = [
   {
@@ -21,6 +23,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage, EventsListPage],
+  entryComponents: [
+    EventsListPage
+]
 })
 export class ProfilePageModule {}
